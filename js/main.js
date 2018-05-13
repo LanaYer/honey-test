51f12e7c507f79344fdc8a6401dac8d39fb29410
LanaYer <yermolaeva.lana@gmail.com>
2018-05-13 12:51:17 +0300

    validate form

--- js/main.js
+++ js/main.js
@@ -1,19 +1,35 @@
 function addComment () {
 
-    $.ajax({
-        type: "POST",
-        data:{
-            'name': $("#formName").val(),
-            'email': $("#formEmail").val(),
-            'comment': $("#formText").val()
-        },
-        url: "ajax/add_comment.php",
-        async: true,
-        success: function(data){
-            if (data) {
-                location.reload();
-            }
-        }
-    });
+    var valid = true;
+    var email = $("#formEmail").val();
+    var name = $("#formName").val();
+    var text = $("#formText").val();
 
+    if (!(email.includes('@')&& email.includes('.'))){
+        valid = false;
+        alert('Проверьте корректность заполнения ввода e-mail!');
+    }
+
+    if (!(email&&name&&text)){
+        valid = false;
+        alert('Заполнены не все поля!');
+    }
+
+    if (valid){
+        $.ajax({
+            type: "POST",
+            data:{
+                'name': name,
+                'email': email,
+                'comment': text
+            },
+            url: "ajax/add_comment.php",
+            async: true,
+            success: function(data){
+                if (data) {
+                    location.reload();
+                }
+            }
+        });
+    }
 }
